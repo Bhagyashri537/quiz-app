039b3212a1c9a26438d02627e8fb7eb7ec694927
Bhagyashri <bhagyashrideoghare116@gmail.com>
2024-08-06 19:38:50 +0530

    home page is done

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,39 @@ import Footer from "./components/Footer";
 import Home from "./components/Pages/Home";
 import Quiz from "./components/Pages/Quiz";
 import Result from "./components/Pages/Result";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [name, setName] = useState("");
+  const [question, setQuestion] = useState();
+  const [score, setScore] = useState(0);
+  const fetchQuestions = async (category = "", difficulty = "") => {
+    const { data } = await axios.get(
+      `https://opentdb.com/api.php?amount=10${
+        category ? `&category=${category}` : ""
+      }${difficulty ? `&difficulty=${difficulty}` : ""}&type=multiple`
+    );
+    setQuestion(data.results);
+  };
+
   return (
     <BrowserRouter>
       <div className="app">
         <Header />
         <main>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/quiz" element={<Quiz/>} />
+            <Route
+              path="/"
+              element={
+                <Home
+                  name={name}
+                  setName={setName}
+                  fetchQuestions={fetchQuestions}
+                />
+              }
+            />
+            <Route path="/quiz" element={<Quiz name={name} questions={question} score={score} setScore={setScore} setQuestion={setQuestion}/>} />
             <Route path="/result" element={<Result />} />
           </Routes>
         </main>
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
+import Img from "../../assets/title.jpg";
+import "./Home.css";
+import Categories from "../Data/Categories";
+import { useNavigate } from "react-router-dom";
 
-function Home() {
+function Home({ name, setName, fetchQuestions }) {
+  const [category, setCategory] = useState("");
+  const [error, setError] = useState(false);
+  const [difficulty, setDifficulty] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    if (!category || !name) {
+      setError(true);
+    } else {
+      setError(false);
+      fetchQuestions(category, difficulty)
+      navigate("/quiz");
+    }
+  };
+
   return (
-    <div>Home</div>
-  )
+    <div className="content">
+      <div className="title">
+        <span style={{ fontSize: 30, fontWeight: 400 }}>
+          Let's Play & Learn
+        </span>
+        <div className="settings">
+          {error && <p style={{ color: "red" }}>Please fill in all fields</p>}
+          <input
+            type="text"
+            placeholder="Enter Your Name"
+            className="inputfield"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <select
+            id="options"
+            onChange={(e) => setCategory(e.target.value)}
+            value={category}
+            className="category"
+          >
+            <option value="" disabled>
+              Select category
+            </option>
+            {Categories.map((cat, index) => (
+              <option key={index} value={cat.value}>
+                {cat.category}
+              </option>
+            ))}
+          </select>
+          <select onChange={(e) => setDifficulty(e.target.value)} value={difficulty} className="category">
+          <option value="" disabled>Select Difficulty</option>
+  <option value="easy">Easy</option> 
+  <option value="medium">Medium</option> 
+  <option value="hard">Hard</option>
+          </select>
+          <button className="btn" onClick={handleSubmit}>
+            Let's Start
+          </button>
+        </div>
+      </div>
+      <img src={Img} className="img" alt="Title" />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
